b968f3f8125f494b0de49e4de6a96bb41cb5b259
Rahul Shah <rashah1998@yahoo.com>
2022-02-14 11:08:55 -0500

    commit tree 12d2fcca9048bd335957a27474c6117ddbb0285f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+*/
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+//wrapGifts(gifts);
+
+function writeCards(arr, event) {
+    const messages = [];
+    for (let i = 0; i < arr.length; i++) {
+        messages.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+//console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+    return '';
+}
+
+//console.log(countDown(10));
